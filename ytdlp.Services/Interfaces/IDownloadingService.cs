5d381431fa8b5e32d99e302535fc4169f5766483
mf450s <85843054+mf450s@users.noreply.github.com>
2025-12-29 21:58:51 +0100

    feat: add optional cookieFile parameter to IDownloadingService

--- ytdlp.Services/Interfaces/IDownloadingService.cs
+++ ytdlp.Services/Interfaces/IDownloadingService.cs
@@ -2,5 +2,12 @@ namespace ytdlp.Services.Interfaces;
 
 public interface IDownloadingService
 {
-    Task TryDownloadingFromURL(string url, string configFile);
+    /// <summary>
+    /// Attempts to download content from a URL using the specified configuration and optional cookie file.
+    /// </summary>
+    /// <param name="url">The URL to download from.</param>
+    /// <param name="configFile">The name of the configuration file to use.</param>
+    /// <param name="cookieFile">Optional: The name of the cookie file to use for authentication.</param>
+    /// <returns>A task representing the asynchronous download operation.</returns>
+    Task TryDownloadingFromURL(string url, string configFile, string? cookieFile = null);
 }
